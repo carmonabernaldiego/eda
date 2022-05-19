685045f835687de810f2ba8465fcc0d6dbee385c
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-05-18 21:54:30 -0500

    update

--- src/HashTable/Main.java
+++ src/HashTable/Main.java
@@ -5,83 +5,80 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class Main {
-    static int iterador = 0;
-    static Hashtable<String, Object> usuarios=new Hashtable<String, Object>();
-
-    public static void agregarUsuario( ){
-        String nombre,direccion;
-        int edad;
-        nombre = JOptionPane.showInputDialog("Ingresa tu nombre: ");
-        edad = Integer.parseInt( JOptionPane.showInputDialog("Ingresa tu edad: ") );
-        direccion = JOptionPane.showInputDialog("Ingresa tu dirección: ");
-
-        Persona crearUsuario = new Persona(nombre,edad,direccion);
-        usuarios.put( (iterador+""), crearUsuario );
-        iterador++;
-
-    }
-
-    public static void mostrarUsuarios( ){
-        Enumeration nombres;
-        nombres = usuarios.keys();
-        String clave;
-
-        String impresion ="*********************************\n";
-        while (nombres.hasMoreElements()){
-            clave = (String) nombres.nextElement();
-            impresion +="Clave: "+clave + "\n" + usuarios.get(clave);
-            impresion += "\n*********************************\n";
-        }
-        JOptionPane.showMessageDialog(null,impresion);
-    }
-
-    public static void reemplazarUsuario(){
-        String key = JOptionPane.showInputDialog("Ingrese la clave de la persona a modificar: ");
-
-        String nombre,direccion;
-        int edad;
-        nombre = JOptionPane.showInputDialog("Ingresa tu nombre: ");
-        edad = Integer.parseInt( JOptionPane.showInputDialog("Ingresa tu edad: ") );
-        direccion = JOptionPane.showInputDialog("Ingresa tu dirección: ");
-
-        Persona crearUsuario = new Persona(nombre,edad,direccion);
-
-        usuarios.replace(key,crearUsuario);
-
-
-    }
-
-    public static void eliminarUsuario(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese la clave de la persona a eliminar: ");
-        String key = sc.nextLine();
-        usuarios.remove(key);
-    }
+    static int i = 0;
+    static Hashtable<String, Object> personal = new Hashtable<String, Object>();
 
     public static void main(String[] args) {
+        int opc = 0;
+
         Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
         do {
-            System.out.println("Ingrese la opción deseada: \n" +
-                    "1. Agregar Usuario\n" +
-                    "2. Mostrar Usuarios\n" +
-                    "3. Remplazar Usuario\n" +
-                    "4. Eliminar Usuario");
-           int opc = sc.nextInt();
+            System.out.println("Ingrese una opcion: \n" +
+                    "1.- Agregar Usuario\n" +
+                    "2.- Mostrar Usuarios\n" +
+                    "3.- Remplazar Usuario\n" +
+                    "4.- Eliminar Usuario\n" +
+                    "5.- Salir");
+
+           opc = sc.nextInt();
 
            switch (opc){
                case 1 ->{
-                   agregarUsuario( );
+                   String nombre, direccion;
+                   int edad;
+
+                   System.out.println("Ingresa tu nombre: ");
+                   nombre = sc2.nextLine();
+                   System.out.println("Ingresa tu edad: ");
+                   edad = sc.nextInt();
+
+                   Persona persona = new Persona(nombre, edad);
+
+                   personal.put((i+""), persona);
+                   i++;
                }
                case 2 ->{
-                   mostrarUsuarios();
+                   Enumeration nombres;
+                   String id;
+
+                   nombres = personal.keys();
+
+                   String impresion = "\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+
+                   while (nombres.hasMoreElements()){
+                       id = (String) nombres.nextElement();
+
+                       impresion +="ID: " + id + "\n" + personal.get(id);
+                       impresion += "\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+                   }
+                   System.out.println(impresion);
                }
                case 3 ->{
-                   reemplazarUsuario();
+                   String nombre;
+                   int edad;
+
+                   System.out.println("Ingrese el ID del personal a modificar: ");
+                   String id = sc2.nextLine();
+                   System.out.println("Ingresa tu nombre: ");
+                   nombre = sc2.nextLine();
+                   System.out.println("Ingresa tu edad: ");
+                   edad = sc.nextInt();
+
+                   Persona persona = new Persona(nombre, edad);
+
+                   personal.replace(id, persona);
                }
                case 4 ->{
-                   eliminarUsuario();
+                   System.out.println("Ingrese el ID del personal a eliminar: ");
+                   String id = sc.nextLine();
+                   personal.remove(id);
+               }
+               case 5 -> {
+                   System.out.println("Adios...");
                }
            }
-        }while( true );
+        }while(opc != 5);
     }
 }
--- src/HashTable/Persona.java
+++ src/HashTable/Persona.java
@@ -1,21 +1,18 @@
+package HashTable;
+
 public class Persona {
-    //Atributos
     String nombre;
     int edad;
-    String direccion;
 
-
-    public Persona(String nombre, int edad, String direccion) {
+    public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
-        this.direccion = direccion;
     }
 
     @Override
     public String toString() {
         return
                 "Nombre: " + nombre + "\n" +
-                "Edad:" + edad + "\n" +
-                "Direccion: " + direccion;
+                "Edad:" + edad;
     }
 }
