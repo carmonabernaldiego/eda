8dbb78d10a2f5eafce3d954479261f1de08369a9
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-06-23 11:18:49 -0500

    update

--- src/ArbolPractica/MainArbol.java
+++ src/ArbolPractica/MainArbol.java
@@ -6,17 +6,18 @@ public class MainArbol {
     public static void main(String[] argumentos) {
         Arbol arbol = new Arbol();
 
-        arbol.insertar(10);
-        arbol.insertar(6);
         arbol.insertar(15);
-        arbol.insertar(3);
-        arbol.insertar(8);
-        arbol.insertar(12);
-        arbol.insertar(20);
         arbol.insertar(6);
+        arbol.insertar(20);
+        arbol.insertar(3);
         arbol.insertar(9);
-        arbol.insertar(30);
+        arbol.insertar(18);
+        arbol.insertar(17);
+        arbol.insertar(24);
+        arbol.insertar(1);
+        arbol.insertar(4);
         arbol.insertar(7);
+        arbol.insertar(12);
 
         int opc = 0;
 
